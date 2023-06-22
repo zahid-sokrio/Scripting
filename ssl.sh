f7452dc2441787a39254ddbf9bd49f191ef4395e
Zahid <122356540+zahid-sokrio@users.noreply.github.com>
2023-06-22 12:52:28 +0600

    Create SSL Certificate

---
+++ ssl.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "Note: You will need to deploy a DNS TXT record to your domain."
+
+echo "Enter your domain names (domain.com)..."
+read domain
+
+echo "Enter your email address..."
+read email
+
+echo "Installing SSL for ${domain}..."
+sudo certbot certonly \
+    --manual \
+    --preferred-challenges=dns \
+    --email ${email} \
+    --server https://acme-v02.api.letsencrypt.org/directory \
+    --agree-tos \
+    -d "*.${domain}"
+
+##Generate PFX 
+##openssl pkcs12 -export -out certificate.pfx -inkey privkey.pem -in fullchain.pem -certfile cert.pem
